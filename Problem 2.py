4bc0dd6534c236eda42c6f733fc901c94b41b1ee
jjaenim <joshuatead@gmail.com>
2023-04-28 19:07:22 +0800

    Reading the file line by line

--- Problem 2.py
+++ Problem 2.py
@@ -3,4 +3,7 @@
 # Assignment 4 - Problem 2
 
 # Open the file
-with open("C:/assignments_oop/TRYYY/student_data.txt", "r") as file_handle:
+with open("C:\assignments_oop\A4-Problem2/student_data.txt", "r") as file_handle:
+    
+    # Read the file line by line
+    lines = file_handle.readlines()
