00c73b0c31f099d3875fdfd918e85e878572279c
jjaenim <joshuatead@gmail.com>
2023-04-28 19:01:20 +0800

    Pseudocode

--- Problem 2.py
+++ Problem 2.py
@@ -1,3 +1,17 @@
 # Joshua Adrian O. Daet
 # BSCpE 1-4
 # Assignment 4 - Problem 2
+
+# Pseudocode
+
+# Open the file
+# Read the file line by line
+# Initialize the highest GWA and name
+# Iterate over the lines
+# Split the line into name and GWA
+# Convert the GWA to float
+# Compare the GWA with the highest GWA
+# Update the highest GWA and name
+# Handle the case where GWA is not a valid float
+# Print the result
+# Creating column for the names and GWA
