4cb6fbeda0a19c46cf269c3342c1de96891d8888
jjaenim <joshuatead@gmail.com>
2023-04-28 19:05:09 +0800

    Opening the input file (student_data.txt)

--- Problem 2.py
+++ Problem 2.py
@@ -2,16 +2,5 @@
 # BSCpE 1-4
 # Assignment 4 - Problem 2
 
-# Pseudocode
-
 # Open the file
-# Read the file line by line
-# Initialize the highest GWA and name
-# Iterate over the lines
-# Split the line into name and GWA
-# Convert the GWA to float
-# Compare the GWA with the highest GWA
-# Update the highest GWA and name
-# Handle the case where GWA is not a valid float
-# Print the result
-# Creating column for the names and GWA
+with open("C:/assignments_oop/TRYYY/student_data.txt", "r") as file_handle:
