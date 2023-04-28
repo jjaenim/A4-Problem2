f45d5a67276d42889647c1557d3ee2816b663384
jjaenim <joshuatead@gmail.com>
2023-04-28 19:08:19 +0800

    Initialize the highest GWA and name

--- Problem 2.py
+++ Problem 2.py
@@ -7,3 +7,7 @@ with open("C:\assignments_oop\A4-Problem2/student_data.txt", "r") as file_handle
     
     # Read the file line by line
     lines = file_handle.readlines()
+
+# Initialize the highest GWA and name
+highest_gwa = float('-inf')
+highest_name = ""
